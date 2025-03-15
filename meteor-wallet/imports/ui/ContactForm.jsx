caad1a82bab06cb672a93718f44a47ba88cf91e5
cameron <chum0001@student.monash.edu>
2025-03-15 21:58:40 +1100

    Added remove contact functionality

--- meteor-wallet/imports/ui/ContactForm.jsx
+++ meteor-wallet/imports/ui/ContactForm.jsx
@@ -1,22 +1,41 @@
 import React, {useState} from 'react';
 import {Meteor} from "meteor/meteor";
+import { ErrorAlert } from './components/ErrorAlert';
+import { SuccessAlert } from './components/SuccessAlert';
 
 export const ContactForm = () => {
 
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [imageUrl, setImageUrl] = useState("");
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+    
+    const showError = ({message}) => {
+      setError(message)
+      setTimeout(() => {
+        setError("")
+      }, 3000)
+    }
 
+    const showSuccess = ({message}) => {
+      setSuccess(message)
+      setTimeout(() => {
+        setSuccess("")
+      }, 3000)
+    }
 
     
     const saveContact = () =>{
         Meteor.call('contacts.insert',{name, email, imageUrl}, (errorResponse) => {
           if(errorResponse){
-            alert(errorResponse.error);
+            showError({message: errorResponse.error});
+            setError(errorResponse.error);
           } else{
               setName("");
               setEmail("");
               setImageUrl("");
+              showSuccess({ message: "Contact saved." });
           }
         });
 
@@ -24,6 +43,8 @@ export const ContactForm = () => {
 
     return(
       <form className = "mt-6">
+        {error && <ErrorAlert message = {error}/>}
+        {success && <SuccessAlert message = {success}/>}
         <div className = "grid grid-cols-6 gap-6">
           <div className = "col-span-6 sm:col-span-6 lg:col-span-2">
             <label htmlFor='name' className = "block text-sm font-medium text-gray-700">
